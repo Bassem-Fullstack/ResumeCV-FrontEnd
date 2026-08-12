adf3722c087e1a41aa21bc39b883efc91fdba95b
Bassem_Fullstack <bassemali102345@gmail.com>
2026-08-12 22:15:17 +0300

    do something broooooo😜

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*", // أي طلب يبدأ بـ api/
-        destination: "https://resume-cv-backend-nou7.vercel.app/:path*", // سيتحول لرابط الباك إند الحقيقي
-      },
-    ];
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
